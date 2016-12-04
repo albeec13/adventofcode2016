996e3ef8e6317fd5cfa8be8d26723f77ccf5259b
Albert Chaharbakhshi <albeec13@gmail.com>
2016-12-03 23:49:11 -0600

    Day 2, Puzzle 2

--- days/2/main.go
+++ days/2/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
    "fmt"
+   "encoding/hex"
+   "strings"
 )
 
 var keySeq = []string{"UULDRRRDDLRLURUUURUURDRUURRDRRURUDRURRDLLDRRRDLRUDULLRDURLULRUUURLDDRURUDRULRDDDUDRDLDDRDDRUURURRDDRLRLUDLUURURLULLLRRDRLDRLRDLULULRDRDDUURUDRRURDLRRDDDLUULDURDLDLLRLRLLUDUDLRDDLUURUUDDRDULDDLDLLDULULRLDDDUDDDRLLRURLRDUUUDUUDDURRDLDDLRDLLUDDLDRLDULDRURLUUDLURLUDRULRLRUUUURLUUUDDULLRLLURDRURLLRLRLDDRURURULRULLUUUULUDULDDDRDDLURLUURRLDDRDRUDDRRLURRDURRLDUULRRLLRDLLDDUURULLRUURRRRDRRURLULLRLRDDULULRDLDDLULLD",
@@ -9,17 +11,17 @@ var keySeq = []string{"UULDRRRDDLRLURUUURUURDRUURRDRRURUDRURRDLLDRRRDLRUDULLRDUR
 
 type KeyPad struct {
     CurrPos int
-    Code []int
+    Code []byte
 }
 
 func main() {
-    kp := KeyPad{5,make([]int,len(keySeq))}
+    kp := KeyPad{5,make([]byte,len(keySeq))}
 
     for index,seq := range keySeq {
         kp.Move(index,seq)
     }
 
-    fmt.Println(kp.Code)
+    fmt.Println(strings.ToUpper(strings.Join(strings.Split(hex.EncodeToString(kp.Code),"0"),"")))
 }
 
 func (kp *KeyPad) Move(index int, seq string) {
@@ -27,26 +29,30 @@ func (kp *KeyPad) Move(index int, seq string) {
         switch char {
         case 'U':
             switch kp.CurrPos {
-            case 4,5,6,7,8,9:
-                kp.CurrPos -= 3
+            case 3,13:
+                kp.CurrPos -= 2
+            case 6,7,8,10,11,12:
+                kp.CurrPos -= 4
             }
         case 'D':
             switch kp.CurrPos {
-            case 1,2,3,4,5,6:
-                kp.CurrPos += 3
+            case 1,11:
+                kp.CurrPos += 2
+            case 2,3,4,6,7,8:
+                kp.CurrPos += 4
             }
         case 'R':
             switch kp.CurrPos {
-            case 1,2,4,5,7,8:
+            case 2,3,5,6,7,8,10,11:
                 kp.CurrPos += 1
             }
         case 'L':
             switch kp.CurrPos {
-            case 2,3,5,6,8,9:
+            case 3,4,6,7,8,9,11,12:
                 kp.CurrPos -= 1
             }
         }
     }
 
-    kp.Code[index] = kp.CurrPos
+    kp.Code[index] = byte(kp.CurrPos)
 }
