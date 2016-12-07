1bd70058ebaff89a4b3c625540a8a811c9a2bed7
Albert Chaharbakhshi <albeec13@gmail.com>
2016-12-07 03:08:41 -0600

    Day 6, Puzzle 1

---
+++ days/6/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var runeCnt []map[rune]int
+    var message []byte
+
+    inputSlice := strings.Split(input, "\n")
+    runeCnt = make([]map[rune]int,len(inputSlice[0]))
+    message = make([]byte,len(inputSlice[0]))
+
+    for i,_ := range runeCnt {
+        runeCnt[i] = make(map[rune]int)
+    }
+
+    for _,row := range inputSlice {
+        for i,char := range row {
+            runeCnt[i][char]++
+        }
+    }
+
+    for i,column := range runeCnt {
+        max := 0
+        letter := 'a' - 1
+        for key,val := range column {
+            if val > max {
+                letter = key
+                max = val
+            }
+        }
+        message[i] = byte(letter)
+    }
+
+    fmt.Println(string(message))
+}
