a95ec67bbdc709dec72dd9f161a4b292e4470c69
Albert Chaharbakhshi <albeec13@gmail.com>
2016-12-09 11:03:24 -0600

    Day 7, Puzzle 2

--- days/7/main.go
+++ days/7/main.go
@@ -8,6 +8,7 @@ import (
 
 func main () {
     cnt := 0
+    cnt2 := 0
 
 Top:
     for _,in := range strings.Split(input, "\n") {
@@ -45,6 +46,10 @@ Top:
             }
         }
 
+        if hasABA_BAB(inBrack, outBrack) {
+            cnt2++
+        }
+
         for _,str := range inBrack {
             if hasABBA(str) {
                 continue Top
@@ -58,20 +63,59 @@ Top:
             }
         }
     }
-    fmt.Println(cnt)
+    fmt.Println("ABBA: ", cnt)
+    fmt.Println("ABA:  ", cnt2)
 }
 
 func hasABBA(s string) bool {
-    ret := false
-
     if len(s) >= 4 {
         for i := 4; i <= len(s); i++ {
             q := s[i-4:i]
             if (q[0] == q[3]) && (q[1] == q[2]) && (q[0] != q[1]) { return true }
         }
     }
+    return false
+}
 
-    return ret
+func hasABA_BAB(in []string, out []string) bool {
+    foundIn := make([]string,0)
+
+    for _,s := range in {
+        foundIn = append(foundIn, hasABA(s)...)
+    }
+
+    for _,s := range out {
+        foundOut := hasABA(s)
+
+        for _,a := range foundOut {
+            if len(a) == 3 {
+                inv := []byte{' ',' ',' '}
+                inv[0] = a[1]
+                inv[2] = a[1]
+                inv[1] = a[0]
+
+                for _,i := range foundIn {
+                    if(string(inv) == i) { return true }
+                }
+
+            }
+        }
+    }
+    return false
+}
+
+func hasABA(s string) []string {
+    found := make([]string,0)
+
+    if len(s) >= 3 {
+        for i := 3; i <= len(s); i++ {
+            q := s[i-3:i]
+            if (q[0] == q[2]) && (q[0] != q[1]) {
+                found = append(found, q)
+            }
+        }
+    }
+    return found
 }
 
 func tern(comp bool, valTrue string, valFalse string) string {
